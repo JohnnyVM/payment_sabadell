272b1fe7267eeb41bebf25be3ca40d70c57d6e71
johnny <vmjuan90@gmail.com>
2020-08-31 23:57:59 +0200

    Useless instalable status

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Payment adquirer Sabadell",
+    "category": "Payment Acquirer",
+    "summary": "Payment Acquirer: Sabadell Implementation",
+    "version": "0.1.0",
+    "author": "Juan Vázquez Moreno <vmjuan90@gmail.com>",
+    "depends": ["payment", "website_sale"],
+    "data": [
+        'views/views.xml',
+        'data/data.xml'
+        ],
+    "license": "AGPL-3",
+    "installable": True,
+}
