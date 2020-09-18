ef8fb058fb0fffd919c9dfb05179d124f9017e83
johnny <vmjuan90@gmail.com>
2020-09-18 20:12:11 +0200

    functional status 3

--- __init__.py
+++ __init__.py
@@ -2,3 +2,8 @@
 
 from . import models
 from . import controllers
+
+from odoo.addons.payment import reset_payment_provider
+
+def uninstall_hook(cr, registry):
+    reset_payment_provider(cr, registry, 'sabadell')
